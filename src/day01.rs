a64dd43f63afea8c046e3c4a0f4157b059b469ce
meltinglava <meltinglavaoutland@gmail.com>
2024-12-01 07:09:47 +0100

    inital commit 2024

---
+++ src/day01.rs
@@ -0,0 +1,25 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+
+#[aoc_generator(day1)]
+pub fn input_generator(input: &str) -> Vec<String> {
+    todo!()
+}
+
+
+#[aoc(day1, part1)]
+fn part1(input: &[String]) -> usize {
+    todo!()
+}
+
+// #[aoc(day1, part2)]
+// fn part2(input: &[String]) -> usize {
+//     tood!()
+// }
+
+#[cfg(test)]
+mod tests {
+    use indoc::indoc;
+
+    use super::*;
+}
---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+mod day01;
+
+extern crate aoc_runner;
+
+#[macro_use]
+extern crate aoc_runner_derive;
+
+aoc_lib! { year = 2024 }
